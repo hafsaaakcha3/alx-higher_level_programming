d441421aa6782d73605046c655259bda3c4c9044
hafsaaakcha3 <hafsaaakcha3@gmail.com>
2023-12-02 06:50:17 -0800

    0

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,65 +1,41 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * len - Calculates the length of a linked list.
- * @head: A pointer to the head of the list.
- * Return: The length of the list.
+ * is_palindrome - Check if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: 1 if the linked list is a palindrome, 0 otherwise.
  */
-int len(listint_t **head)
-{
-	listint_t *current = *head;
-	int i = 0;
 
-	while (current != NULL)
-	{
-		i = i + 1;
-		current = current->next;
-	}
-	return i;
-}
-
-/**
- * navigate - Navigates to a specific node in the linked list.
- * @head: The head of the list.
- * @len: The position to navigate to.
- * Return: The pointer to the desired node.
- */
-listint_t *navigate(listint_t *head, int len)
-{
-	listint_t *current = head;
-
-	for (int i = 1; i <= len && current != NULL; i++)
-		current = current->next;
-
-	return current;
-}
-
-/**
- * is_palindrome - Checks if a linked list is a palindrome.
- * @head: A pointer to the head of the list.
- * Return: 1 if it's a palindrome, 0 otherwise.
- */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *self = *head, *mirror = *head, *prev = NULL, *nextNode = NULL;
+	int length = 0, i = 0;
 
-	if (current == NULL)
-		return 1;
-
-	int i = 1;
-	int length = len(&current);
-	int loopedlen = length / 2;
-	listint_t *mirror;
-
-	while (loopedlen != 0)
+	while (mirror && mirror->next)
 	{
-		mirror = navigate(current, (length - i));
-		if (mirror->n != current->n)
-			return 0;
-		current = current->next;
-		loopedlen = loopedlen - 1;
-		i = i + 2;
+		length += 2;
+		mirror = mirror->next->next;
 	}
-	return 1;
+	if (mirror != NULL)
+		length++;
+	mirror = *head;
+	for (i = 0; i < length / 2; i++)
+		mirror = mirror->next;
+	while (mirror)
+	{
+		nextNode = mirror->next;
+		mirror->next = prev;
+		prev = mirror;
+		mirror = nextNode;
+	}
+	mirror = prev;
+	while (mirror)
+	{
+		if (self->n != mirror->n)
+			return (0);
+		self = self->next;
+		mirror = mirror->next;
+	}
+	return (1);
 }
